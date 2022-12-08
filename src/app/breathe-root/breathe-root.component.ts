bcda14b79eccb249caf7809c2af6a8857c80b25b
Dan Schnau <dan.schnau@skykick.com>
2022-12-08 16:41:17 -0500

    init box-breathing

--- src/app/breathe-root/breathe-root.component.ts
+++ src/app/breathe-root/breathe-root.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+class BreathState {
+  public fullness: number = 0
+}
 
 @Component({
   selector: 'app-breathe-root',
   templateUrl: './breathe-root.component.html',
   styleUrls: ['./breathe-root.component.css']
 })
-export class BreatheRootComponent {
+export class BreatheRootComponent implements OnInit {
 
+  protected breathState:BreathState = new BreathState();
+
+  ngOnInit(): void {
+    this.breathState = {
+      fullness: 0,
+    };
+  }
+
+  protected start(): void {
+    this.inhale(4);
+  }
+
+  protected hold(seconds: number) {
+    setTimeout(() => {
+      if(this.breathState.fullness <= 0) {
+        this.inhale(4);
+      } else {
+        this.exhale(4);
+      }
+    }, 1000*seconds);
+  }
+
+  protected inhale(seconds: number): void {
+    this.breathState.fullness = 0;
+    let interval = 10/seconds;
+    let intervalId = setInterval(() => {
+      this.breathState.fullness += interval;
+      if (this.breathState.fullness > 100) {
+        clearInterval(intervalId);
+        this.hold(4);
+      }
+    }, 100)
+  }
+  
+  protected exhale(seconds: number): void {
+    this.breathState.fullness = 100;
+    let interval = 10/seconds;
+    let intervalId = setInterval(() => {
+      this.breathState.fullness -= interval;
+      if (this.breathState.fullness < 0) {
+        clearInterval(intervalId);
+        this.hold(4);
+      }
+    }, 100)
+  }
 }
